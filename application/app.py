6742f63f298d360242003fa50b63228c9b5e441f
Kyel Harwell <138349999+quantumGrail@users.noreply.github.com>
2024-03-31 15:09:15 +0000

    Define navigation routes and configure SQL

--- application/app.py
+++ application/app.py
@@ -1,15 +1,23 @@
 # Import Management
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, session
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure Application
 app = Flask(__name__)
 
+# Configure SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test-flex.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+# Configure appication SQLite database
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
