53fe9c3a42c30c93366b1db3237ca7e3c2eee5b9
Kyel Harwell <138349999+quantumGrail@users.noreply.github.com>
2024-04-27 19:34:22 +0000

    Seperate test control bar from list

--- application/templates/static/scripts.js
+++ application/templates/static/scripts.js
@@ -4,21 +4,32 @@ function addTest() {
     window.location.href = `/add_test`;
 }
 
-// Search Tests
 function searchTests() {
-    console.log("Search function called");
-    var input, filter, cards, card, i, testName, testDescription;
+    var input, cards, card, i, testName, testDescription, noResultsMessage;
     input = document.getElementById("searchInput").value.toUpperCase();
     cards = document.getElementById("tests-list").getElementsByClassName("card");
+    noResultsMessage = document.getElementById("noResultsMessage");
+
+    // Flag to keep track of whether any matches are found
+    var foundMatches = false;
+
     for (i = 0; i < cards.length; i++) {
         card = cards[i];
-        testName = card.querySelector(".card-title").textContent.toUpperCase();
-        testDescription = card.querySelector(".card-text").textContent.toUpperCase();
+        testName = card.querySelector(".card-header strong").textContent.toUpperCase();
+        testDescription = card.querySelector(".card-body .card-text:first-of-type").textContent.toUpperCase();
         if (testName.indexOf(input) > -1 || testDescription.indexOf(input) > -1) {
             card.style.display = "";
+            foundMatches = true; // Set foundMatches to true if at least one match is found
         } else {
             card.style.display = "none";
         }
+    }
+
+    // Display or hide the "No results found" message based on whether any matches were found
+    if (!foundMatches) {
+        noResultsMessage.style.display = "block"; // Show the message
+    } else {
+        noResultsMessage.style.display = "none"; // Hide the message
     }
 }
 
