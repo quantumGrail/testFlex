06fb4aec07697b5383a9160a518a40debe82882a
Kyel Harwell <138349999+quantumGrail@users.noreply.github.com>
2024-05-05 06:00:29 +0000

    Fix test search... again

--- application/templates/static/scripts.js
+++ application/templates/static/scripts.js
@@ -4,42 +4,46 @@ function addTest() {
     window.location.href = `/add_test`;
 }
 
-// Search Tests
+// Search Existing Tests and Descriptions
 function searchTests() {
     var input, cards, card, i, testName, testDescription, noResultsMessage;
     input = document.getElementById("searchInput").value.toUpperCase();
     cards = document.getElementById("tests-list").getElementsByClassName("card");
     noResultsMessage = document.getElementById("noResultsMessage");
 
-    // Flag to keep track of whether any matches are found
     var foundMatches = false;
 
     for (i = 0; i < cards.length; i++) {
         card = cards[i];
-        testName = card.querySelector(".card-header h5").textContent.toUpperCase(); // Update the selector for the card title
-        testDescription = card.querySelector(".card-body .scrollable-text").textContent.toUpperCase(); // Update the selector for the test description
+        let header = card.querySelector(".card-header h5");
+        if (header) {
+            testName = header.textContent.toUpperCase();
+        } else {
+            testName = ""; 
+        }
+    
+        testDescriptionElement = card.querySelector(".scrollable-text");
+        if (testDescriptionElement) {
+            testDescription = testDescriptionElement.textContent.toUpperCase();
+        } else {
+            testDescription = ""; 
+        }
+        
         if (testName.indexOf(input) > -1 || testDescription.indexOf(input) > -1) {
             card.style.display = "";
-            foundMatches = true; // Set foundMatches to true if at least one match is found
+            foundMatches = true; 
         } else {
             card.style.display = "none";
         }
     }
-
-    // Display or hide the "No results found" message based on foundMatches
+    
     if (foundMatches) {
         noResultsMessage.style.display = "none";
     } else {
         noResultsMessage.style.display = "block";
     }
-
-    // Display or hide the "No results found" message based on whether any matches were found
-    if (!foundMatches) {
-        noResultsMessage.style.display = "block"; // Show the message
-    } else {
-        noResultsMessage.style.display = "none"; // Hide the message
-    }
 }
+
 
 // Edit Existing Test
 function editTest(testId) {
